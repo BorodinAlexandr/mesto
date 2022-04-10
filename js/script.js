34bf6378cf97a348ae90787ec3b6959a19827ea8
Borodin Alexandr <bvachtang@gmail.com>
2022-04-10 18:55:13 +0300

    пофиксить баги ревью

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-const openBtn = document.querySelector(".profile__edit"),
-  closeBtn = document.querySelector(".popup__close"),
-  popup = document.querySelector(".popup"),
-  formElement = document.querySelector(".popup__form"),
-  nameInput = formElement.querySelector(".popup__name"),
-  descrInput = formElement.querySelector(".popup__descr"),
-  personName = document.querySelector(".profile__name"),
-  descr = document.querySelector(".profile__descr");
+const openBtn = document.querySelector('.profile__edit'),
+  closeBtn = document.querySelector('.popup__close'),
+  popup = document.querySelector('.popup'),
+  formElement = document.querySelector('.popup__form'),
+  nameInput = formElement.querySelector('.popup__name'),
+  descrInput = formElement.querySelector('.popup__descr'),
+  personName = document.querySelector('.profile__name'),
+  descr = document.querySelector('.profile__descr');
 
 function openPopup() {
-  popup.classList.add("popup_opened");
-  nameInput.value = personName.innerText;
-  descrInput.value = descr.innerText;
+  popup.classList.add('popup_opened');
+  nameInput.value = personName.textContent;
+  descrInput.value = descr.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
-
-openBtn.addEventListener("click", openPopup);
-
-closeBtn.addEventListener("click", closePopup);
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -33,4 +29,8 @@ function formSubmitHandler(event) {
   closePopup();
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
+openBtn.addEventListener('click', openPopup);
+
+closeBtn.addEventListener('click', closePopup);
+
+formElement.addEventListener('submit', formSubmitHandler);
