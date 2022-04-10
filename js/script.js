7dace06365ed16ebd623f73bb2164eda6bab2e88
Borodin Alexandr <bvachtang@gmail.com>
2022-04-10 19:32:47 +0300

    починить скрипт

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const openBtn = document.querySelector('.profile__edit'),
   closeBtn = document.querySelector('.popup__close'),
   popup = document.querySelector('.popup'),
   formElement = document.querySelector('.popup__form'),
-  nameInput = formElement.querySelector('.popup__name'),
-  descrInput = formElement.querySelector('.popup__descr'),
+  nameInput = formElement.querySelector('.popup__text_value_name'),
+  descrInput = formElement.querySelector('.popup__text_value_descr'),
   personName = document.querySelector('.profile__name'),
   descr = document.querySelector('.profile__descr');
 
